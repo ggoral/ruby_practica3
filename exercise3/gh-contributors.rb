0c630ca631523979c2d95f9b7aa82aa7d24580a8
ggoral <goral.gonzalo@gmail.com>
2013-11-05 11:10:07 -0300

    Read Sawyer resources

--- exercise3/gh-contributors.rb
+++ exercise3/gh-contributors.rb
@@ -8,5 +8,5 @@ user = Octokit.user 'ggoral'
 puts user.name
 
 repo = Octokit.repo 'TTPS-ruby/capacitacion-ruby-ttps'
-puts repo.rels[:contributors].get.data
+puts repo.rels[:contributors].get.data.attrs[:username]
 
