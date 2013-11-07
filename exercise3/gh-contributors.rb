4e96a31d322bf30976e2b41b5d42f92f72364169
ggoral <goral.gonzalo@gmail.com>
2013-11-07 19:30:52 -0300

    finish exercise3

--- exercise3/gh-contributors.rb
+++ exercise3/gh-contributors.rb
@@ -8,5 +8,9 @@ user = Octokit.user 'ggoral'
 puts user.name
 
 repo = Octokit.repo 'TTPS-ruby/capacitacion-ruby-ttps'
-puts repo.rels[:contributors].get.data.attrs[:username]
-
+array = repo.rels[:contributors].get.data
+array.each do |sawyer| 
+  login = sawyer.to_hash[:login]
+  contributions = sawyer.to_hash[:contributions]
+  puts "* #{login} (#{contributions})"
+end
