9f2863298dc69a47354a3c8520922f771650a3c8
ggoral <goral.gonzalo@gmail.com>
2013-12-04 09:55:05 -0300

    add exercise 12_3

---
+++ exercise12_3/app.rb
@@ -0,0 +1,29 @@
+require 'bundler'
+Bundler.require :default, ENV['RACK_ENV'].to_sym
+
+helpers do
+  def form(params, &block)
+    if [:post,:get].include?(params[:method])
+      form_begin = "<form method=\"#{params[:method]}\" action=\"#{params[:action]}\">"
+      form_content = "block.call"
+      return form_begin + form_content + "</form>"
+    end
+
+    "<form method=\"post\" action=\"#{params[:action]}\">
+      <input type=\"hidden\" name=\"_method\" value=\"#{params[:method]}\"/>
+    </form>"
+  end
+
+  def submit(value)
+    "<input type=\"submit\" value=#{value}>"
+  end
+end
+
+get '/' do
+  erb :index
+end
+
+get '/login' do
+  form method: :post, action: '/login', { submit('Entrar') }
+end
+
