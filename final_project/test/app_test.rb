e60ba26addeaab340dc138b90151638ffca105fa
ggoral <goral.gonzalo@gmail.com>
2013-12-17 19:40:06 -0300

    add test resource and resources

--- final_project/test/app_test.rb
+++ final_project/test/app_test.rb
@@ -7,14 +7,21 @@ class AppTest < Minitest::Test
     Sinatra::Application
   end
 
-  def test_get_a_user
+  def test_resources
     server_response = get '/resources/1'
     assert_equal 200, last_response.status
 
     json = JSON.parse server_response.body
     puts json
     puts json['name']
-
   end
 
+  def test_a_resource
+    server_response = get '/resources/1'
+    assert_equal 200, last_response.status
+
+    json = JSON.parse server_response.body
+    puts json
+    puts json['name']
+  end
 end
