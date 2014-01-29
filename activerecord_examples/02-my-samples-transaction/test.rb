854ff9092ac8d8f19fd2dd8c64f5c6a00e55ac69
ggoral <goral.gonzalo@gmail.com>
2014-01-29 13:34:48 -0300

    meeting the source

---
+++ activerecord_examples/02-my-samples-transaction/test.rb
@@ -0,0 +1,25 @@
+Bundler.require
+require_relative 'transaction'
+
+ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: '/tmp/transaction.db'
+
+puts "All transaction names"
+Transaction.all.each do |transaction|
+  p transaction.name
+end
+
+puts "Add a Personal transaction"
+t = Transaction.new
+t.name = "personal"
+t.save
+
+puts "All transaction ids"
+Transaction.all.each do |transaction|
+  p transaction
+end
+
+last_personal_transaction = Transaction.where(name: "personal").order('id desc').first
+p last_personal_transaction
+
+low_id_transactions = Transaction.where( " id < 3 ")
+p low_id_transactions
