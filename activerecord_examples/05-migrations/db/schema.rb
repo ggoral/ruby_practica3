c1d661d41d63440148d798937a95353e59b89bda
ggoral <goral.gonzalo@gmail.com>
2014-02-01 23:32:22 -0300

    Fight with Migrations

--- activerecord_examples/05-migrations/db/schema.rb
+++ activerecord_examples/05-migrations/db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140201230733) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.integer  "amount"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
