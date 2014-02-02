a28f1237d447de19d635fe9abd8fa9963705791a
ggoral <goral.gonzalo@gmail.com>
2014-02-02 00:47:25 -0300

    Add sample migrations

---
+++ activerecord_examples/05-migrations/db/migrate/20140201230733_create_categories.rb
@@ -0,0 +1,16 @@
+# 20140201230733_create_categories.rb
+class CreateCategories < ActiveRecord::Migration
+  def self.up
+    create_table :categories do |t|
+      t.string :name
+      t.integer :amount
+ 
+      t.timestamps
+    end
+  end
+ 
+  def self.down
+    drop_table :categories
+  end
+end
+
