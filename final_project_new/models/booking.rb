d66ade53c85717ef30a3b8f8cfb86e2585a22ccf
ggoral <goral.gonzalo@gmail.com>
2014-02-08 20:39:07 -0300

    add models

---
+++ final_project_new/models/booking.rb
@@ -0,0 +1,16 @@
+class Booking < ActiveRecord:Base
+  belongs_to :resource
+  belongs_to :user
+
+  before_validation :set_pending_status, on: :create
+
+  validates :resource, :start, :end, presence: true
+  validates :status, inclusion: ["pending", "approved"]
+
+  #validates_format_of :user, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+
+  protected
+  def set_pending_status
+    self.status = "pending"
+  end
+end
